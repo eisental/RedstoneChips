f291645c0c281e8e24d2fd4fe7a783f807037d5a
Tal Eisenberg <eisental@gmail.com>
2011-01-26 22:04:26 +0100

    Added chat colors to preferences file and reorganized PrefsManager some more.

--- src/main/java/org/tal/redstonechips/PrefsManager.java
+++ src/main/java/org/tal/redstonechips/PrefsManager.java
@@ -23,30 +23,48 @@ import org.yaml.snakeyaml.Yaml;
  * @author Tal Eisenberg
  */
 public class PrefsManager {
+    private static final String defaultsFileName = "/defaultprefs.yml";
+    private static final String prefsFileName = "preferences.yml";
+
+    private static final String cBTKey = "chipBlockType";
+    private static final String inpBTKey = "inputBlockType";
+    private static final String oBTKey = "outputBlockType";
+    private static final String intBTKey = "interactionBlockType";
+
+    private static final String iCKey = "infoColor";
+    private static final String eCKey = "errorColor";
+    private static final String dCKey = "debugColor";
+
     private RedstoneChips rc;
     private DumperOptions prefDump;
     private Map<String,Object> prefs;
 
-    private Material chipBlockType = Material.SANDSTONE;
-    private Material inputBlockType = Material.IRON_BLOCK;
-    private Material outputBlockType = Material.GOLD_BLOCK;
-    private Material interactionBlockType = Material.LAPIS_BLOCK;
+    private Material chipBlockType;
+    private Material inputBlockType;
+    private Material outputBlockType;
+    private Material interactionBlockType;
 
-    private ChatColor infoColor = ChatColor.GREEN;
-    private ChatColor errorColor = ChatColor.RED;
-    private ChatColor debugColor = ChatColor.AQUA;
+    private ChatColor infoColor;
+    private ChatColor errorColor;
+    private ChatColor debugColor;
+
+    private Map<String, Object> defaults;
 
     public PrefsManager(RedstoneChips plugin) {
         this.rc = plugin;
 
         prefDump = new DumperOptions();
         prefDump.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+        Yaml yaml = new Yaml();
+
+        defaults = (Map<String, Object>) yaml.load(getClass().getResourceAsStream(defaultsFileName));
     }
 
     public void loadPrefs() {
         if (!rc.getDataFolder().exists()) rc.getDataFolder().mkdir();
 
-        File propFile = new File(rc.getDataFolder(), "preferences.yml");
+        File propFile = new File(rc.getDataFolder(), prefsFileName);
         if (!propFile.exists()) { // create empty file if doesn't already exist
             try {
                 propFile.createNewFile();
@@ -59,30 +77,25 @@ public class PrefsManager {
             Yaml yaml = new Yaml(prefDump);
             prefs = (Map<String, Object>)yaml.load(new FileInputStream(propFile));
             if (prefs==null) prefs = new HashMap<String, Object>();
+            loadDefaultsIfNeeded(inpBTKey, oBTKey, cBTKey, iCKey, eCKey, dCKey);
 
-            if (prefs.containsKey("inputBlockType")) {
-                Material i = findMaterial(prefs.get("inputBlockType"));
-                if (i==null) rc.log(Level.WARNING, "Unknown material: " + prefs.get("inputBlockType"));
-                else inputBlockType = i;
-            } else prefs.put("inputBlockType", inputBlockType.name());
+            try {
+                inputBlockType = findMaterial(prefs.get(inpBTKey));
+                outputBlockType = findMaterial(prefs.get(oBTKey));
+                interactionBlockType = findMaterial(prefs.get(intBTKey));
+                chipBlockType = findMaterial(prefs.get(cBTKey));
+            } catch (IllegalArgumentException ie) {
+                rc.log(Level.SEVERE, "While loading preferences: " + ie.getMessage());
+            }
 
-            if (prefs.containsKey("outputBlockType")) {
-                Material i = findMaterial(prefs.get("outputBlockType"));
-                if (i==null) rc.log(Level.WARNING, "Unknown material: " + prefs.get("outputBlockType"));
-                else outputBlockType = i;
-            } else prefs.put("outputBlockType", outputBlockType.name());
 
-            if (prefs.containsKey("chipBlockType")) {
-                Material i = findMaterial(prefs.get("chipBlockType"));
-                if (i==null) rc.log(Level.WARNING, "Unknown material: " + prefs.get("chipBlockType"));
-                else chipBlockType = i;
-            } else prefs.put("chipBlockType", chipBlockType.name());
-
-            if (prefs.containsKey("interactionBlockType")) {
-                Material i = findMaterial(prefs.get("interactionBlockType"));
-                if (i==null) rc.log(Level.WARNING, "Unknown material: " + prefs.get("interactionBlockType"));
-                else interactionBlockType = i;
-            } else prefs.put("interactionBlockType", interactionBlockType.name());
+            try {
+                infoColor = ChatColor.valueOf((String)prefs.get(iCKey));
+                errorColor = ChatColor.valueOf((String)prefs.get(eCKey));
+                debugColor = ChatColor.valueOf((String)prefs.get(dCKey));
+            } catch (IllegalArgumentException ie) {
+                rc.log(Level.SEVERE, "While loading preferences: " + ie.getMessage());
+            }
 
             yaml.dump(prefs, new FileWriter(propFile));
         } catch (IOException ex) {
@@ -93,7 +106,7 @@ public class PrefsManager {
     public void savePrefs() {
         if (!rc.getDataFolder().exists()) rc.getDataFolder().mkdir();
 
-        File prefsFile = new File(rc.getDataFolder(), "preferences.yml");
+        File prefsFile = new File(rc.getDataFolder(), prefsFileName);
         if (!prefsFile.exists()) { // create empty file if doesn't already exist
             try {
                 prefsFile.createNewFile();
@@ -163,16 +176,24 @@ public class PrefsManager {
         return debugColor;
     }
 
-    private static Material findMaterial(Object m) {
+    public Map<String, Object> getPrefs() {
+        return prefs;
+    }
+
+    private static Material findMaterial(Object m) throws IllegalArgumentException{
         if (m instanceof String)
             return Material.getMaterial(((String)m).toUpperCase());
         else if (m instanceof Integer)
             return Material.getMaterial((Integer)m);
-        else return null;
+        else throw new IllegalArgumentException("Invalid material: " + m);
     }
 
-    public Map<String, Object> getPrefs() {
-        return prefs;
+
+    public void loadDefaultsIfNeeded(String... keys) {
+        for (String key : keys) {
+            if (!prefs.containsKey(key))
+                prefs.put(key, defaults.get(key));
+        }
     }
 
 }
